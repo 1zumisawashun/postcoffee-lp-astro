22097273632c69de82f92c12a02241cfad792275
1zumisawashun <shun.1zumisawa@gmail.com>
2022-12-17 16:45:48 +0900

    fix

--- src/components/models/landing/Youtube.tsx
+++ src/components/models/landing/Youtube.tsx
@@ -1,13 +1,19 @@
-import styled from "styled-components";
-import { FlexCard } from "./FlexCard";
-import { recommends } from "../../../functions/constants/recommends";
+import YouTube, { YouTubeProps } from "react-youtube";
 
 export const Youtube = () => {
-  return (
-    <>
-      {recommends.map((recommend) => (
-        <FlexCard card={recommend}></FlexCard>
-      ))}
-    </>
-  );
+  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  const opts: YouTubeProps["opts"] = {
+    width: "100%",
+    height: "650",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+
+  return <YouTube videoId="y-eh4xx3jPQ" opts={opts} onReady={onPlayerReady} />;
 };
